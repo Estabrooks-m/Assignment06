5bb49bfa714af9663d7228966d75ed187d93fb76
Estabrooks <estabrooks.m@northeastern.edu>
2024-03-17 18:47:49 -0400

    rectangle child class

--- Geometric_Shapes.py
+++ Geometric_Shapes.py
@@ -7,16 +7,41 @@ Created on Sun Mar 17 18:40:20 2024
 #parent class
 class shape:
     def area(self, height, width):
+        area= print("Area")
+        return(area)
+    def perimeter(self, height, width):
+        per= print("Perimeter")
+        return(per)
+    
+#child classes
+class circle(shape):
+    None
+    
+class rectangle(shape):
+    def area(self, height, width):
         area= height*width
         return(area)
     def perimeter(self, height, width):
         per= (2*height)+(2*width)
         return(per)
     
-#child classes
-class circle(shape):
+class triangle(shape):
+    None
     
-class rectangle(shape):
     
-class (triangle):
+def main(): 
+    width= 3
+    height= 4
+    shape= rectangle()
+    print(shape.area(height, width))
+    
+    
+    
+    
+    
+    
+    
+main()
+
+
     
