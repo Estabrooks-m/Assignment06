ea582dae0a8534d3d2da58de9dfca68f382d8c79
Estabrooks <estabrooks.m@northeastern.edu>
2024-03-18 16:16:31 -0400

    Added comments

--- Employee_Managment.py
+++ Employee_Managment.py
@@ -9,19 +9,24 @@ class employee:
     def __init__(self, name, position):
         self.name= name
         self.position= position
+    #Prints name and position of employee
     def calculate_salary(self):
         sal= f"{self.name}, {self.position}: "
         return(sal)
-    
+
+#hourly employee child class
 class HourlyEmployee(employee):
+    #overrides parent class salary def for hourly
     def calculate_salary(self):
         pay= float(input("Hourly pay: "))
         hours= float(input("Hours worked: "))     
         salery= pay*hours
         s= round (salery, 2)
         return(s)
-   
+
+#salary employee child class
 class SalariedEmployee(employee):
+    #overrides parent class salary def for salary
     def calculate_salary(self):
         pay= float(input("Pay per year: "))
         years= float(input("Years worked: "))    
@@ -29,8 +34,10 @@ class SalariedEmployee(employee):
         salery= (pay*tax)*years
         s= round (salery, 2)
         return(s)
-    
+
+#Commisioned employee child class
 class CommissionEmployee(employee):
+    #overrides parent class salary def for commision
     def calculate_salary(self):
         company= float(input("How much did you make in sales: "))
         percent= float(input("What percent do you get to take home: "))  
@@ -45,15 +52,18 @@ class CommissionEmployee(employee):
 def main():
     
     emp= "Y"
-    
+    #While user wants to check another empolyee loop
     while (emp == "Y" or emp == "y"):
+        #User enters basic information to send to parent class
         name= str(input("Name: "))
         position= str(input("Position: "))
         styp= str(input("How are you payed: Hourly (H), Salaried (S), Commision (C)? "))
         
+        #if user input does not match wanted input make them try again
         while (styp != "H" and styp != "S" and styp != "C"):
             styp= str(input("Invalid Input, try again "))
         
+        #Depending on which employee use polymorphism to call implement their salary 
         if (styp== "H"):
             employee= HourlyEmployee(name, position)
             print("$", employee.calculate_salary())
@@ -64,6 +74,7 @@ def main():
             employee= CommissionEmployee(name, position)
             print("$", employee.calculate_salary())       
         
+        #Ask if they want to check another employee
         emp= input("Do you want to check another employee? ")
     
 main()
